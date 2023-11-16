eee6a7933f96c1f45104b026274c40dde85e189c
Kevin Patto <kevp2010@gmail.com>
2023-11-15 19:27:29 -0600

    models filled out

---
+++ models/Thought.js
@@ -0,0 +1,39 @@
+const { Schema, model } = require('mongoose');
+const Reaction = require('./Reaction');
+
+const thoughtSchema = new Schema(
+	{
+		thoughtText: {
+			type: String,
+			required: true,
+			minLength: 1,
+			maxLength: 280,
+		},
+		createdAt: {
+			type: Date,
+			default: new Date.now,
+			get: function (date) { return date.toDateString() },
+		},
+		username: {
+			type: String,
+			required: true,
+		},
+		reactions: [Reaction],
+	},
+	{
+		toJSON: {
+			getters: true,
+			virtuals: true,
+		},
+		id: false,
+	}
+);
+
+thoughtSchema.virtual('reactionCount')
+	.get(function () {
+		return this.reactions.length;
+	});
+
+const Thought = model('thought', thoughtSchema);
+
+module.exports = Thought;
